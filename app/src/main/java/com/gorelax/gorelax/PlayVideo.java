b87c0d671a1c837091006aa71504e7385688de9c
annette-l <annette3277@gmail.com>
2018-09-15 17:22:15 -0400

    Initial commit

---
+++ app/src/main/java/com/gorelax/gorelax/PlayVideo.java
@@ -0,0 +1,60 @@
+package com.gorelax.gorelax;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayVideo extends AppCompatActivity {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+
+        videoView = findViewById(R.id.videoView);
+        final MediaController mediacontroller = new MediaController(this);
+        mediacontroller.setAnchorView(videoView);
+
+        videoView.setMediaController(mediacontroller);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.shapes);
+        videoView.requestFocus();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        videoView.setMediaController(mediacontroller);
+                        mediacontroller.setAnchorView(videoView);
+
+                        //mp.setLooping(true);
+
+                    }
+                });
+            }
+        });
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                //Toast.makeText(getApplicationContext(), "Video over", Toast.LENGTH_SHORT).show();
+                videoView.start();
+
+            }
+
+        });
+
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Log.d("API123", "What " + what + " extra " + extra);
+                return false;
+            }
+        });
+    }
+}
